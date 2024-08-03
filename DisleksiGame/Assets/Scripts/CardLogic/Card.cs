0afcea91223c07691a6296ebfb04ee402a27e9c9
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2024-08-03 14:04:20 +0300

    create first card

--- DisleksiGame/Assets/Scripts/CardLogic/Card.cs
+++ DisleksiGame/Assets/Scripts/CardLogic/Card.cs
@@ -9,6 +9,9 @@ public class Card : MonoBehaviour
     public Sprite Icon; // 2D grafik kullandýðýmýz için sprite sýnýfýný çaðýrmamýz lazým
     private bool isOpen; // Kart açýk mý? (bu durumu ancak kartýn kendisi deðiþtirebilmeli, dýþarýdan deðiþtirilmemeli o yüzden private)
 
+    public GameObject CardBack;
+    public GameObject CardFront;
+
     //ama dýþarýdaki nesneler tarafýndan açýk olup olmadýðýnýn anlaþýlmasý lazým bu yüzden public bir property tanýmlanmalý
     public bool IsOpen
     {
@@ -18,7 +21,7 @@ public class Card : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -29,6 +32,14 @@ public class Card : MonoBehaviour
 
     void OnMouseDown()
     {
-        Debug.Log("mouse down");
+        OpenCard();
     }
+
+    public void OpenCard() // cardback' i gizleyip, cardfront' u göstermemiz lazým
+    {
+        isOpen = true; // kartýn açýk olduðu artýk bilinsin
+        CardBack.SetActive(false); // sahnedeki gameobject' i aktif ve inaktif yapmamýzý saðlýyor
+        CardFront.SetActive(true);
+    }
+
 }
